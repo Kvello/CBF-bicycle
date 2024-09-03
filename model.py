395e28099957f151db89d3d48240c703df111b81
Kvello <markus.kv1@gmail.com>
2024-09-03 18:16:43 +0200

    Fix model

--- model.py
+++ model.py
@@ -11,8 +11,8 @@ def bicycle(t,x:np.ndarray, u:np.ndarray) -> np.ndarray:
     x_dot: derivative of the state vector
     """
     x_dot = np.zeros(5)
-    x_dot[0] = x[2]*np.cos(x[2])
-    x_dot[1] = x[2]*np.sin(x[2])
+    x_dot[0] = x[2]*np.cos(x[3])
+    x_dot[1] = x[2]*np.sin(x[3])
     x_dot[2] = u[0]
     x_dot[3] = x[4]
     x_dot[4] = u[1]
